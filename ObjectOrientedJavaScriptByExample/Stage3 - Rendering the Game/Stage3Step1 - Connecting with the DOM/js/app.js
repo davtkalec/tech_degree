97e00ee51f6368b05cdbaaeba6c428b71860e64f
davtkalec <davtkalec@gmail.com>
2019-03-13 16:17:14 +0100

    final_version

--- ObjectOrientedJavaScriptByExample/Stage3 - Rendering the Game/Stage3Step1 - Connecting with the DOM/js/app.js
+++
@@ -1,8 +0,0 @@
-const game = new Game();
-
-document.getElementById('begin-game').addEventListener('click',function(){
-    game.startGame();
-
-    this.style.display = 'none';
-    document.getElementById('play-area').style.opacity = '1';
-});
